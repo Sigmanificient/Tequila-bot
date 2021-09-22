7256ec2601b0fded03fbdeaff7a60cc24bb00e40
Yohann Boniface <edhyjox@gmail.com>
2021-09-22 21:11:43 +0200

    :sparkles: more utilities functions

--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,17 @@
+from time import time
+
+HALF_HOUR: int = 1800
+
+
 def word_capitalize(word: str) -> str:
     """Capitalize the first letter of every word within a sentence."""
     return ' '.join(map(str.capitalize, word.split()))
+
+
+def get_int(string):
+    return int(''.join(ch for ch in string if ch.isdigit()))
+
+
+def get_last_half_hour():
+    current_sec = time()
+    return int(current_sec - (current_sec % HALF_HOUR))
