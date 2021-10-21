13e01c60e871ac629a40688eb72094a147a1338b
Yohann Boniface <edhyjox@gmail.com>
2021-10-21 06:27:23 +0200

    :recycle: Converting base bot to pincer bot

--- app/bot.py
+++ app/bot.py
@@ -1,8 +1,8 @@
-from typing import Optional, NoReturn, Tuple
+from typing import Optional, Tuple
 
 import dotenv
-from discord import Guild, Activity, ActivityType
-from discord.ext import commands
+from discord import Guild
+from pincer import Client
 
 GUILD_ID: int = 888527538710777876
 LOADED_EXTENSIONS: Tuple[str, ...] = (
@@ -10,27 +10,22 @@ LOADED_EXTENSIONS: Tuple[str, ...] = (
 )
 
 
-class Bot(commands.Bot):
+class Bot(Client):
 
     def __init__(self) -> None:
-        super().__init__(command_prefix='/')
+        super().__init__(dotenv.dotenv_values('.env').get('TOKEN'))
         self.guild: Optional[Guild] = None
 
-        self.remove_command('help')
-
         for ext in LOADED_EXTENSIONS:
-            self.load_extension(f'app.cogs.{ext}')
+            self.load_cog(f'app.cogs.{ext}')
 
     async def on_ready(self) -> None:
         self.guild = self.get_guild(GUILD_ID)
         print(self.user, 'is ready')
 
-        await self.change_presence(
-            activity=Activity(
-                type=ActivityType.watching,
-                name=f"{self.command_prefix}help"
-            )
-        )
-
-    def run(self) -> NoReturn:
-        super().run(dotenv.dotenv_values('.env').get('TOKEN'))
+        # await self.change_presence(
+        #    activity=Activity(
+        #        type=ActivityType.watching,
+        #        name=f"{self.command_prefix}help"
+        #    )
+        # )
