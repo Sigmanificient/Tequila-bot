a7ad1f9fdaf841e7f0d9566ce03b7ff433089e14
sigmanificient <edhyjox@gmail.com>
2022-01-12 23:39:58 +0100

    :recycle: Recycle members.py for pinccer

--- app/cogs/members.py
+++ app/cogs/members.py
@@ -1,19 +1,19 @@
 from typing import NoReturn, Optional
 
-from discord import TextChannel
-from discord.ext import commands
-from discord.ext.commands import Context, CommandError
+from pincer import Client, command
+from pincer.objects import TextChannel, MessageContext, InteractionFlags
+
 
 from app.bot import Bot
 from app.classes.members_list import MemberList
-from app.exceptions import MemberAlreadyExists, MemberNotFound
+from app.exceptions import MemberAlreadyExists, MemberNotFound, BotError
 from app.utils import SALARIED_ROLE_ID, PDG_ROLE_ID
 
 CHANNEL_ID: int = 888563799701991435
 MESSAGE_ID: int = 888770216480366632
 
 
-class MembersCog(commands.Cog):
+class MembersCog:
     """A simple commands cog template."""
 
     def __init__(self, client: Bot):
@@ -23,7 +23,7 @@ class MembersCog(commands.Cog):
         self.channel: Optional[TextChannel] = None
         self.manager = None
 
-    @commands.Cog.listener()
+    @Client.event
     async def on_ready(self):
         self.channel = self.client.guild.get_channel(CHANNEL_ID)
 
@@ -31,79 +31,81 @@ class MembersCog(commands.Cog):
             await self.channel.fetch_message(MESSAGE_ID),
         )
 
-    @commands.command(
+    @command(
         name='addpeople',
         description="Ajoute une personne dans la liste des adhérents"
     )
-    @commands.has_any_role(SALARIED_ROLE_ID, PDG_ROLE_ID)
-    async def add_people_command(self, ctx: Context, person_name: str):
-        await ctx.message.delete()
+    async def add_people_command(self, person_name: str):
         await self.manager.add(person_name, 'people')
-        await ctx.send('Ajouté!', delete_after=2)
+        return (
+            'Ajouté!',
+            InteractionFlags.EPHEMERAL
+        )
 
-    @commands.command(
+    @command(
         name='addcomp',
         description="Ajoute une entreprise dans la liste des adhérents"
     )
-    @commands.has_any_role(SALARIED_ROLE_ID, PDG_ROLE_ID)
-    async def add_company_command(self, ctx: Context, company_name: str):
-        await ctx.message.delete()
+    async def add_company_command(self, company_name: str):
         await self.manager.add(company_name, 'company')
-        await ctx.send('Ajouté!', delete_after=2)
+        return (
+            'Ajouté!',
+            InteractionFlags.EPHEMERAL
+        )
 
-    @commands.command(
+    @command(
         name='removepeople',
         description="Retire la personne donnée de la liste des adhérents"
     )
-    @commands.has_any_role(SALARIED_ROLE_ID, PDG_ROLE_ID)
-    async def remove_people_command(self, ctx: Context, company_name: str):
-        await ctx.message.delete()
+    async def remove_people_command(self, company_name: str):
         await self.manager.remove(company_name, 'people')
-        await ctx.send('Retiré!', delete_after=2)
+        return (
+            'Retiré!',
+            InteractionFlags.EPHEMERAL
+        )
 
-    @commands.command(
+    @command(
         name='removecomp',
         description="Retire l'entreprise donnée de la liste des adhérents"
     )
-    @commands.has_any_role(SALARIED_ROLE_ID, PDG_ROLE_ID)
-    async def remove_company_command(self, ctx: Context, company_name: str):
-        await ctx.message.delete()
+    async def remove_company_command(self, company_name: str):
         await self.manager.remove(company_name, 'company')
-        await ctx.send('Retiré!', delete_after=2)
+        return (
+            'Retiré!',
+            InteractionFlags.EPHEMERAL
+        )
 
-    @commands.command(
+    @command(
         name="desc-2",
         description="Met à jour la description de la liste des adherents."
     )
-    @commands.has_any_role(SALARIED_ROLE_ID, PDG_ROLE_ID)
-    async def set_drink_list_description(self, ctx: Context, *, message: str):
-        await ctx.message.delete()
-
+    async def set_drink_list_description(self, *, message: str):
         with open(
             "assets/members_description.txt",
             'w', encoding='utf-8'
         ) as f:
             f.write(message)
 
-        await ctx.send(
-            f"Description mise à jour\n>>> {message}", delete_after=5
-        )
-
         await self.manager.update()
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx: Context, exc: CommandError):
-        if not isinstance(exc, commands.errors.CommandInvokeError):
+        return (
+            f"Description mise à jour\n>>> {message}",
+            InteractionFlags.EPHEMERAL
+        )
+
+    @Client.event
+    async def on_command_error(self, exc: Exception):
+        if not isinstance(exc, BotError):
             return
 
-        if isinstance(exc.original, MemberAlreadyExists):
-            await ctx.send(
-                f"Le membre `{exc.original.member_name}`"
+        if isinstance(exc, MemberAlreadyExists):
+            return (
+                f"Le membre `{exc.member_name}`"
                 f"est déjà enregistrée dans `{exc.original.member_type}`"
             )
 
         if isinstance(exc.original, MemberNotFound):
-            await ctx.send(
+            return (
                 f"Le membre `{exc.original.member_name}`"
                 f"n'est pas dans `{exc.original.member_type}`"
             )
